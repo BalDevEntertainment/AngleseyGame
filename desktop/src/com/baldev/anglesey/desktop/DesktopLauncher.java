651a897649ae51cb3c58e5a2f8ee739e950174c1
ariel <baldeventertainment@gmail.com>
2017-05-14 14:10:39 -0300

    Initial commit

---
+++ desktop/src/com/baldev/anglesey/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.baldev.anglesey.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.baldev.anglesey.AngleseyGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new AngleseyGame(), config);
+	}
+}
